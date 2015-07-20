507b21814fd52edf89548a168d37e58e37297ed1
Rich Churcher <rich.churcher@gmail.com>
2015-07-20 12:39:45 +1200

    Flask scaffold

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
